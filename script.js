65a75e7cbb478f7e88a03f63734fddc42fb5d766
TexturePackPorts <151673163+TexturePackPorts@users.noreply.github.com>
2023-12-06 16:43:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+emailjs.init("vVANUSC1OrdkemATM"); // Replace with your EmailJS User ID
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail() {
+    // Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // Send the email using EmailJS
+    emailjs.send("service_dn8r4n7", "template_ec8op2x", {
+        to_email: to,
+        subject: subject,
+        message: message
+    })
+        .then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
+}
